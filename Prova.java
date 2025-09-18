bfc435cf758bd0b1ba743fbed1cec81d038b3410
val10val <val.acc001@gmail.com>
2025-09-18 07:29:36 +0200

    Add files via upload
    caricamento

---
+++ Prova.java
@@ -0,0 +1,8 @@
+// Java Program to demonstrate the
+// syntax of the main() function
+class Prova {
+    public static void main(String[] args)
+    {
+        System.out.println("hola mundo");
+    }
+}
